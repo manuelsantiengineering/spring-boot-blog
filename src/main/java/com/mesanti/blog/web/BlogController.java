e1b9fccf3748a86dca82a26edb69746121ed128f
Santiago Laboy <msanti04@harris.com>
2019-04-18 10:45:28 -0400

    First commit

---
+++ src/main/java/com/mesanti/blog/web/BlogController.java
@@ -0,0 +1,19 @@
+package com.mesanti.blog.web;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class BlogController {
+  private Logger logger = LoggerFactory.getLogger(BlogController.class);
+  
+  @GetMapping("/")
+  public String showLandingPage(Model model) {
+    logger.info("This is show home page method ");
+    return "home";
+  }
+  
+}
