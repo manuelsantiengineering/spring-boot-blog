f14e0326a4f69524b228a90c982caae5889d107d
Santiago Laboy <msanti04@harris.com>
2019-04-22 14:05:43 -0400

    Issues with the ElasticSearch Data Configuration.

--- src/main/java/com/mesanti/blog/config/ElasticDataConfig.java
+++ src/main/java/com/mesanti/blog/config/ElasticDataConfig.java
@@ -3,8 +3,13 @@ package com.mesanti.blog.config;
 import java.net.InetAddress;
 
 import org.elasticsearch.client.Client;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.transport.TransportAddress;
+import org.elasticsearch.transport.client.PreBuiltTransportClient;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.elasticsearch.rest.RestClientAutoConfiguration.RestHighLevelClientConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -28,23 +33,27 @@ public class ElasticDataConfig {
 
 	    @Bean
 	    public Client client() throws Exception {
+	    	
+	    	Settings settings = Settings.builder()
+	    	        .put("cluster.name", esClusterName)
+	    	        .put("client.transport.sniff", true).build();
+	    	System.out.println("\n\n\n\n" + "6 - ElasticSearch Host: " + esHost + " Port: "+ esPort + "\n\n\n\n");
+	    	TransportClient transportClient = new PreBuiltTransportClient(settings);
+//	    	transportClient.addTransportAddress(new TransportAddress(InetAddress.getByName("127.0.0.1"), 9300))
+	    	transportClient.addTransportAddress(new TransportAddress(InetAddress.getByName(esHost), esPort));
+//	    	transportClient.addTransportAddress(new TransportAddress(InetAddress.getByName(esHost), 9201));
 
-	        /*Settings esSettings = Settings.builder()
-	                .put("cluster.name", esClusterName)
-	                .build();*/
-	        //https://www.elastic.co/guide/en/elasticsearch/guide/current/_transport_client_versus_node_client.html
-	        TransportClientFactoryBean transportClientFactory = new TransportClientFactoryBean();
-	        transportClientFactory.setClusterName(esClusterName);
-	        transportClientFactory.afterPropertiesSet();
-	        
-	        return transportClientFactory.getObject()
-	                  .addTransportAddress(
-					  new TransportAddress(InetAddress.getByName(esHost), esPort));
+	    	System.out.println("\n\n\n\n" + "7 - ElasticSearch Host: " + esHost + " Port: "+ esPort + "\n\n\n\n" + transportClient.connectedNodes().toString());
+	    	
+	    	return transportClient;
 	    }
 
 	    @Bean
 	    public ElasticsearchTemplate elasticsearchTemplate() throws Exception {
-	        return new ElasticsearchTemplate(client());
+	    	System.out.println("\n\n\n\n" + "1 - ElasticSearch Host: " + esHost + " Port: "+ esPort + "\n\n\n\n");
+	    	Client client = client();
+	    	System.out.println("\n\n\n\n" + "10 - ElasticSearch Host: " + esHost + " Port: "+ client.toString() + "\n\n\n\n" + client.toString() + "\n\n\n\n");
+	        return new ElasticsearchTemplate(client);
 	    }
 
 }
